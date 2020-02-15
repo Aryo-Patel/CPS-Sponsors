433383b0a7040721b784ff8d5105972d4aadba3f
Alexander Nagel <slipshot21@gmail.com>
2020-02-15 16:09:44 -0600

    Add files via upload

--- SHS Hacks 2020/client/src/registrationPage.js
+++ SHS Hacks 2020/client/src/registrationPage.js
@@ -17,6 +17,7 @@ const academicdrop = document.querySelector('#academicDrop')
 const communitydrop = document.querySelector('#communityDrop')
 
 const interestSelect = document.querySelectorAll('.intclick')
+const selected = document.querySelector('#selectedInterests');
 
 let name = ''
 let username = ''
@@ -24,33 +25,46 @@ let password = ''
     //overalldropdown
 interestsWrap.addEventListener('mouseover', function(event) {
     dropContent.style.display = 'block';
-})
+});
+
 interestsWrap.addEventListener('mouseout', function(event) {
     dropContent.style.display = 'none';
-})
-Array.from(interestSelect).forEach(addEventListener('click', function(event) {
+});
 
-        }) stemdrop.addEventListener('mouseover', function(event) {
-            stemwrap.style.display = 'block';
-        }) stemdrop.addEventListener('mouseout', function(event) {
-            stemwrap.style.display = 'none';
-        })
+Array.from(interestSelect).forEach(dropdowns => {
+    dropdowns.addEventListener('click', function(event) {
+        let n = document.createElement('P')
+        let t = document.createTextNode('' + dropdowns.innerHTML);
+        n.appendChild(t)
+        selected.appendChild(n)
+        console.log(selected.children)
+    })
+});
 
-        sportsdrop.addEventListener('mouseover', function(event) {
-            sportsdrop.style.display = 'block';
-        }) sportswrap.addEventListener('mouseout', function(event) {
-            sportswrap.style.display = 'none';
-        })
+stemdrop.addEventListener('mouseover', function(event) {
+    stemwrap.style.display = 'block';
+});
+stemdrop.addEventListener('mouseout', function(event) {
+    stemwrap.style.display = 'none';
+});
 
-        academicdrop.addEventListener('mouseover', function(event) {
-            academicwrap.style.display = 'block';
-        }) academicdrop.addEventListener('mouseout', function(event) {
-            academicwrap.style.display = 'none';
-        })
+sportsdrop.addEventListener('mouseover', function(event) {
+    sportsdrop.style.display = 'block';
+});
+sportswrap.addEventListener('mouseout', function(event) {
+    sportswrap.style.display = 'none';
+});
 
-        submit.addEventListener('click', function(event) {
-            name = nameForm.value
-            username = usernameForm.value
-            password = passwordForm.value
+academicdrop.addEventListener('mouseover', function(event) {
+    academicwrap.style.display = 'block';
+});
+academicdrop.addEventListener('mouseout', function(event) {
+    academicwrap.style.display = 'none';
+});
 
-        })
+submit.addEventListener('click', function(event) {
+    name = nameForm.value
+    username = usernameForm.value
+    password = passwordForm.value
+
+});

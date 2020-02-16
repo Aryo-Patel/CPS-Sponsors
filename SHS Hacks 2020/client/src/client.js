d1d1ca37c3f259b88b5639111ea2233696a35aa9
Alexander Nagel <slipshot21@gmail.com>
2020-02-16 04:45:11 -0600

    Add files via upload

--- SHS Hacks 2020/client/src/client.js
+++ SHS Hacks 2020/client/src/client.js
@@ -5,19 +5,19 @@ localStorage.getItem
 let arr = []
 const sock = io();
 
-form.addEventListener('submit', e =>{
+form.addEventListener('submit', e => {
     e.preventDefault();
 });
 
 btn.addEventListener('click', addToList);
 
 
-function addToList(e){
+function addToList(e) {
     console.log('click fired');
     let text = postText.value;
     arr.push(text);
     localStorage.setItem("Messages", JSON.stringify(arr));
-    if(text !== ''){
+    if (text !== '') {
         postText.value = '';
 
         // let li = document.createElement('li');
@@ -29,22 +29,25 @@ function addToList(e){
 
         // li.append(h1);
         // document.getElementById('posts').appendChild(li);
-
-        sock.emit('textToWrite', text);
+        let user = localStorage.getItem("currentUser");
+        sock.emit('textToWrite', text, user);
     }
 
 }
-function addToListServer(message){
-    console.log('server read');
 
-
+function addToListServer(message, user) {
+    console.log(user);
+    let image = document.createElement('img');
+    image.src = "../images/profilePicture.png";
+    let span = document.createElement('span');
     let li = document.createElement('li');
     li.classList.add('post');
-
+    span.innerHTML = user;
     let h1 = document.createElement('h1');
     h1.classList.add('text');
     h1.innerHTML = message;
-
+    li.appendChild(image);
+    li.appendChild(span);
     li.append(h1);
     document.getElementById('postsection').appendChild(li);
 

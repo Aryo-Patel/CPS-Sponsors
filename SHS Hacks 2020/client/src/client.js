56b17863271d158243dea9ada92039479925cb26
Alexander Nagel <slipshot21@gmail.com>
2020-02-16 07:08:02 -0600

    Add files via upload

--- SHS Hacks 2020/client/src/client.js
+++ SHS Hacks 2020/client/src/client.js
@@ -3,6 +3,8 @@ let btn = document.getElementById('submit');
 let form = document.getElementById('write-text');
 localStorage.getItem
 let arr = []
+let tags = document.querySelectorAll(".tags");
+tagsArray = Array.from(tags);
 const sock = io();
 
 form.addEventListener('submit', e => {
@@ -29,27 +31,71 @@ function addToList(e) {
 
         // li.append(h1);
         // document.getElementById('posts').appendChild(li);
+        let submitTags = tagHolder.map(tag => {
+            return tag.innerHTML;
+        });
+        let textAreaValue = document.getElementById('text-area').value;
+        console.log(textAreaValue);
+        console.log(typeof(submitTags[0]));
+        console.log(`Submit tags before server ${submitTags}`);
         let user = localStorage.getItem("currentUser");
-        sock.emit('textToWrite', text, user);
+        document.getElementById('text-area').value = '';
+        tagHolder.forEach(tag => {
+            tag.style.backgroundColor = '#e6e6e6';
+        })
+        tagHolder = [];
+        sock.emit('textToWrite', text, user, submitTags, textAreaValue);
     }
 
 }
 
-function addToListServer(message, user) {
+
+
+function addToListServer(message, user, tagHolder, textValueArea) {
+    console.log(textValueArea);
+    console.log(`After server tags: ${tagHolder}`);
     console.log(user);
     let image = document.createElement('img');
+    let textBody = document.createElement('p');
+    textBody.innerHTML = textValueArea;
+    textBody.classList.add('text-area');
     image.src = "../images/profilePicture.png";
+    image.classList.add("resize");
     let span = document.createElement('span');
     let li = document.createElement('li');
     li.classList.add('post');
     span.innerHTML = user;
+    span.classList.add("MOVE");
     let h1 = document.createElement('h1');
     h1.classList.add('text');
     h1.innerHTML = message;
     li.appendChild(image);
     li.appendChild(span);
     li.append(h1);
+    li.append(textBody);
+    let spacer = document.createElement('div');
+    spacer.classList.add('spacer');
+    li.append(spacer);
+    tagHolder.forEach(tag => {
+        let holder = document.createElement('span');
+        holder.classList.add('tags');
+        holder.innerHTML = tag;
+        li.append(holder);
+    })
     document.getElementById('postsection').appendChild(li);
+    console.log(li);
+    tagHolder = [];
 
 }
+let tagHolder = [];
+tagsArray.forEach(tag => {
+
+    tag.addEventListener("click", e => {
+        tagHolder.push(tag);
+        console.log(tag);
+        console.log(tagHolder);
+        tag.style.backgroundColor = '#cccccc';
+    });
+
+});
 sock.on('message', addToListServer);

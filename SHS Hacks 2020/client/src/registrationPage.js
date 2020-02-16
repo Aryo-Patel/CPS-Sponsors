288ed70390da677dcd21183aa8626b64821f0c11
Alexander Nagel <slipshot21@gmail.com>
2020-02-15 19:29:37 -0600

    Add files via upload

--- SHS Hacks 2020/client/src/registrationPage.js
+++ SHS Hacks 2020/client/src/registrationPage.js
@@ -2,6 +2,10 @@ const submit = document.querySelector('#submit')
 const usernameForm = document.querySelector('#username')
 const nameForm = document.querySelector('#name')
 const passwordForm = document.querySelector('#password')
+const emailForm = document.querySelector('#email')
+const phonenumberForm = document.querySelector('#phonenumber')
+const streetForm = document.querySelector('#street')
+const twoaddressForm = document.querySelector('#twoaddress')
 const intHeader = document.querySelector('#intHeader')
 const interestsWrap = document.querySelector('#firstInterest')
 const dropContent = document.querySelector('#intDropDown')
@@ -19,10 +23,16 @@ const communitydrop = document.querySelector('#communityDrop')
 const interestSelect = document.querySelectorAll('.intclick')
 const selected = document.querySelector('#selectedInterests');
 let interestArr = []
+let checkArr = []
+let toggle = false;
 
 let name = ''
 let username = ''
 let password = ''
+let email = ''
+let phonenumber = ''
+let street = ''
+let twoaddress = ''
     //overalldropdown
 interestsWrap.addEventListener('mouseover', function(event) {
     dropContent.style.display = 'block';
@@ -34,22 +44,41 @@ interestsWrap.addEventListener('mouseout', function(event) {
 
 Array.from(interestSelect).forEach(dropdowns => {
     dropdowns.addEventListener('click', function(event) {
-        let n = document.createElement('P')
-        let t = document.createTextNode('' + dropdowns.innerHTML);
-        n.appendChild(t)
-        selected.appendChild(n)
-        console.log(selected.children)
-        interestArr.push(dropdowns.innerHTML)
+        console.log('beginning' + toggle)
+        for (i = 0; i < checkArr.length; i++) {
+            if (checkArr[i] == dropdowns.innerHTML) {
+                toggle = true;
+            }
+        }
+        if (!toggle) {
+            console.log(checkArr)
+            let n = document.createElement('P')
+            let t = document.createTextNode('' + dropdowns.innerHTML);
+            n.appendChild(t)
+            selected.appendChild(n)
+            console.log(selected.children)
+            interestArr.push('' + dropdowns.innerHTML)
+            checkArr.push(dropdowns.innerHTML)
+        }
+        toggle = false;
+        console.log('ending' + toggle)
     })
 });
 
 submit.addEventListener('click', function(event) {
+    email = emailForm.value
+    phonenumber = phonenumberForm.value
+    street = streetForm.value
+    twoaddress = twoaddressForm.value
     name = nameForm.value
     username = usernameForm.value
     password = passwordForm.value
-    console.log(name + "," + username + ',' + password);
-    var information = {"password":'', "interests":'', "location":"Wilmette"};
+    var information = { "password": '', "interests": '', "email": '', "phonenumber": '', "street": '', "twoaddress": '' };
     information['password'] = password;
     information['interests'] = interestArr;
+    information['email'] = email;
+    information['phonenumber'] = phonenumber;
+    information['street'] = street;
+    information['twoaddress'] = twoaddress;
     localStorage.setItem(username, JSON.stringify(information));
 });

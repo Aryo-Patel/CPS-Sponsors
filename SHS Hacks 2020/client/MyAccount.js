4fe200971cfd923a8ccbd78eeabb68588ef59383
Alexander Nagel <slipshot21@gmail.com>
2020-02-16 07:05:48 -0600

    Add files via upload

--- SHS Hacks 2020/client/MyAccount.js
+++ SHS Hacks 2020/client/MyAccount.js
@@ -4,3 +4,9 @@ if (JSON.parse(localStorage.getItem(localStorage.getItem("currentUser")))["isDon
 else {
     document.getElementById("headBio").innerHTML = "Recipient";
 }
+if (JSON.parse(localStorage.getItem(localStorage.getItem("currentUser")))["biography"] != ""){
+document.getElementById("biography").innerHTML = JSON.parse(localStorage.getItem(localStorage.getItem("currentUser")))["biography"];
+}
+else {(document.getElementById("biography").innerHTML = "No biography available.");}
+
+document.getElementById("userName").innerHTML = localStorage.getItem("currentUser");

165f151eb1b60a40e48e56446b91d1632b9bc5ca
Alexander Nagel <slipshot21@gmail.com>
2020-02-16 02:36:20 -0600

    Add files via upload

---
+++ SHS Hacks 2020/client/src/Tester.js
@@ -0,0 +1,150 @@
+let information1 = { "isDonor": "true", "name": 'Not Jeff Bezos', "password": 'squarecircle', "interests": ['Athletics'], "email": 'interiorcrocodile', "phonenumber": '22499382672', "street": '638 Node Ave', "twoaddress": 'Glenview, IL, 60022', "biography": 'I am not Jeff Bezos. I do not live in Medina, Washington. I do, however, have money that I want to grant to excellent athletes who suffer from a lack of circumstance' };
+let information2 = { "isDonor": "true", "name": 'Jonathon Gates', "password": 'bigboy', "interests": ['STEM', 'Academic Clubs'], "email": 'largelad@gmail.com', "phonenumber": '8476368293', "street": '821 ThisPlace Ln', "twoaddress": 'Wilmette, IL, 60091', "biography": 'I am an elderly man living in Wilmette hoping to give back to the community by reinvesting in the human capital of bright individuals' };
+let information3 = { "isDonor": "true", "name": 'Shelby Kennedy', "password": 'crunch', "interests": ['STEM', 'Community'], "email": 'liftwarp@gmail.com', "phonenumber": '2245326632', "street": '5554 Prince St', "twoaddress": 'Lake Forest, IL, 60201', "biography": 'I am a professor giving back to my community by inspiring girls in STEM and those with powerful community projects and agendas' };
+let information4 = {
+    "isDonor": "false",
+    "name": 'Computer Science Club',
+    "school": 'New Trier High School',
+    "password": 'transylvania',
+    "donations": [
+        ['money', 'equipment', 'equipment'],
+        [null, 'Laptop', 'Raspberry Pi'],
+        ['800', '2', '5']
+    ],
+    "interests": ['STEM'],
+    "email": 'nicekid@gmail.com',
+    "phonenumber": '3127589933',
+    "street": '479 Anywhere Ln',
+    "twoaddress": 'Glencoe, IL, 60022',
+    "biography": 'I am a senior at New Trier looking for funding and equipment to fuel a passion for coding amongst the students at my school. We currently do not have the equipment to effectively teach or practice coding and we are hoping to find a like-minded sponsor who hopes to foster the importance of technical knowledge within young minds'
+};
+let information5 = {
+    "isDonor": "false",
+    "name": 'Math Team',
+    "school": 'Neverland High School',
+    "password": 'broomhandle',
+    "donations": [
+        ['money', 'equipment', 'equipment'],
+        [null, 'Textbooks', 'Math Tutor'],
+        ['1000', '5', '2']
+    ],
+    "interests": ['STEM'],
+    "email": 'lisagronkowski@gmail.com',
+    "phonenumber": '8476378473',
+    "street": '364 Nowhere St',
+    "twoaddress": 'Chicago, IL, 60022',
+    "biography": 'I am a junior at Neverland seeking money, textbooks, and a couple of volunteers to help improve the existing math team. There are a number of students at my school hoping to compete regionally but they lack the resources and dedicated teaching to succeed, and I want to help them grow'
+};
+let information6 = {
+    "isDonor": "false",
+    "name": 'Science Olympiad',
+    "school": 'Adlai E. Stevenson High School',
+    "password": 'underwear',
+    "donations": [
+        ['money', 'equipment', 'volunteer'],
+        [null, 'Laboratory Equipment', 'Chaperone'],
+        ['5000', '12', '5']
+    ],
+    "interests": ['STEM'],
+    "email": 'verycool@yahoo.com',
+    "phonenumber": '2247368392',
+    "street": '8817 Anywhere Way',
+    "twoaddress": 'Lincolnshire, IL, 60045',
+    "biography": 'I am '
+};
+let information7 = {
+    "isDonor": "false",
+    "name": 'Lacrosse Team',
+    "school": 'Quest High School',
+    "password": 'anamalia',
+    "donations": [
+        ['money', 'equipment'],
+        [null, 'Sticks'],
+        ['300', '8']
+    ],
+    "interests": ['Athletics'],
+    "email": 'anotherplant@gmail.com',
+    "phonenumber": '2248859038',
+    "street": '9008 Goldfish St',
+    "twoaddress": 'Chicago, IL, 60022',
+    "biography": 'I am a senior at Quest looking for funding and equipment to start a small lacrosse team at my school. I and my friends love the sport but cannot gather the funds to rent an indoor space or play with broken equipment.'
+};
+let information10 = {
+    "isDonor": "false",
+    "name": 'Computer Engineering',
+    "school": 'Cheeto High School',
+    "password": 'fruitbythefoot',
+    "donations": [
+        ['money', 'equipment', 'volunteer'],
+        [null, 'Arduinos', 'Laptop'],
+        ['3000', '4', '1']
+    ],
+    "interests": ['STEM'],
+    "email": 'pretzel@yahoo.com',
+    "phonenumber": '224868935',
+    "street": '7324 Krispie St',
+    "twoaddress": 'Chicago, IL, 60045',
+    "biography": 'I am a junior at Cheeto High School trying to move beyond conceptual computer engineering and work with my peers on our first real project, however we lack the resources for our attempt.'
+};
+let information11 = {
+    "isDonor": "false",
+    "name": 'MAGIC STEM for Girls',
+    "school": 'Reign High School',
+    "password": 'banana',
+    "donations": [
+        ['money', 'equipment'],
+        [null, 'Textbooks'],
+        ['600', '10']
+    ],
+    "interests": ['Athletics'],
+    "email": 'anotherplant@gmail.com',
+    "phonenumber": '2248859038',
+    "street": '7329 LipBalm St',
+    "twoaddress": 'Chicago, IL, 60022',
+    "biography": 'I am a senior at Reign trying to inspire freshman and sophomore girls to consider STEM by providing interesting opportunities to engage with the fields, and I am seeking the funding and mentorship for my club/project'
+};
+let information8 = {
+    "isDonor": "false",
+    "name": 'Debate',
+    "school": 'Nadharm High School',
+    "password": 'sleepingbag',
+    "donations": [
+        ['money', 'volunteer'],
+        [null, 'Debate Moderator'],
+        ['500', '1']
+    ],
+    "interests": ['Academic Clubs'],
+    "email": 'cheesecurds@gmail.com',
+    "phonenumber": '8476371273',
+    "street": '364 Nowhere St',
+    "twoaddress": 'Glencoe, IL, 60022',
+    "biography": 'I am a junior at Neverland seeking money, textbooks, and a couple of volunteers to help improve the existing math team. There are a number of students at my school hoping to compete regionally but they lack the resources and dedicated teaching to succeed, and I want to help them grow'
+};
+let information9 = {
+    "isDonor": "false",
+    "name": 'Model UN',
+    "school": 'Arguable High School',
+    "password": 'cokebottle',
+    "donations": [
+        ['money', 'volunteer', 'volunteer'],
+        [null, 'Organizer', 'Chaperone'],
+        ['1500', '1', '3']
+    ],
+    "interests": ['Academic Clubs'],
+    "email": 'oranges@yahoo.com',
+    "phonenumber": '2248889585',
+    "street": '8817 Anywhere Way',
+    "twoaddress": 'Lincolnshire, IL, 60045',
+    "biography": "Although my school doesn't have a huge model UN club, it is my dream to host a large model UN event for the surrounding schools to foster a sense of community and connect students with others excited to pursue political science."
+};
+localStorage.setItem("IBM", JSON.stringify(information1));
+localStorage.setItem("Apple", JSON.stringify(information2));
+localStorage.setItem("McDonalds", JSON.stringify(information3));
+localStorage.setItem("Culvers", JSON.stringify(information4));
+localStorage.setItem("SHOP", JSON.stringify(information5));
+localStorage.setItem("MERK", JSON.stringify(information6));
+ocalStorage.setItem("KR", JSON.stringify(information7));
+localStorage.setItem("NVDA", JSON.stringify(information8));
+localStorage.setItem("MSFT", JSON.stringify(information9));
+localStorage.setItem("RSG", JSON.stringify(information10));
+localStorage.setItem("WMT", JSON.stringify(information11));
